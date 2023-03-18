af56a8c2419a0e1c5a911242955f7b0cb5b15eb6
Cocoandjane <janeliangbc@gmail.com>
2023-03-17 22:58:50 -0700

    j

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
+
+const path = require('path');
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static("static"))
 
 
 app.get('/', (req, res) => {
-  res.render("index")
+  res.sendFile(path.join(__dirname + '/views/index.ejs'));
 })
 
 module.exports = app;
