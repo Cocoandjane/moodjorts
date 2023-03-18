27415cd293fcc187c9dd397538aeac08b3a34694
Cocoandjane <janeliangbc@gmail.com>
2023-03-17 22:11:32 -0700

    gggg

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ app.set('view engine', 'ejs')
 app.use(express.static("static"))
 
 app.get('/', (req, res) => {
-  res.render("index")
+  res.setHeader('Content-Type', 'text/plain');
+  res.render("index.ejs")
 })
 
 module.exports = app;
