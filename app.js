0712bbab1a6051bea2d925584d723793f2721e57
Cocoandjane <janeliangbc@gmail.com>
2023-03-17 22:02:20 -0700

    a

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-// const express = require('express')
-// const app = express()
-// app.set('view engine', 'ejs')
-// app.use(express.static("static"))
-
-
-// app.get('/', (req, res) => {
-//   res.render("index")
-// })
-
-// module.exports = app;
-
-
 const express = require('express')
+
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static("static"))
-const port = 3000
 
 app.get('/', (req, res) => {
   res.render("index")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app;
