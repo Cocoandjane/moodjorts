9bbb21a48c77929765c6b78745f35d526973ddc6
Cocoandjane <janeliangbc@gmail.com>
2023-03-17 23:26:42 -0700

    aw

--- api/index.js
+++ api/index.js
@@ -1,42 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var app = express();
-app.set('view engine', 'ejs')
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("static"))
 
-
-
-
-
-const whitelist = [
-  '*'
-];
-
-app.use((req, res, next) => {
-  const origin = req.get('referer');
-  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-  if (isWhitelisted) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  }
-  // Pass to next layer of middleware
-  if (req.method === 'OPTIONS') res.sendStatus(200);
-  else next();
+// Define route for home page
+app.get('/', (req, res) => {
+  res.render('index');
 });
-
-const setContext = (req, res, next) => {
-  if (!req.context) req.context = {};
-  next();
-};
-app.use(setContext);
-
-app.use('/', (req, res) => {
-    res.render('../views/index.ejs')
-});
-
-
 
 module.exports = app;
