abe827c2e2336cba9e6668cc20988b7fb8e99721
Cocoandjane <janeliangbc@gmail.com>
2023-03-17 21:48:58 -0700

    e

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+// const express = require('express')
+// const app = express()
+// app.set('view engine', 'ejs')
+// app.use(express.static("static"))
+
+
+// app.get('/', (req, res) => {
+//   res.render("index")
+// })
+
+// module.exports = app;
+
+
 const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static("static"))
-
+const port = 3000
 
 app.get('/', (req, res) => {
   res.render("index")
 })
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require('./app.js');
+// const app = require('./app.js');
 
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`server should be running at http://localhost:${PORT}/`))
+// app.listen(PORT, () => console.log(`server should be running at http://localhost:${PORT}/`))
